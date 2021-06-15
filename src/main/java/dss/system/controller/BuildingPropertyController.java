9a9ce187f315e6f97e5734a6eb7f036119458d0f
VolodymyrS <volodymyr.stepura@gmail.com>
2021-06-15 14:35:48 +0300

    added controllers

---
+++ src/main/java/dss/system/controller/BuildingPropertyController.java
@@ -0,0 +1,9 @@
+package dss.system.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/building/property")
+public class BuildingPropertyController {
+}
